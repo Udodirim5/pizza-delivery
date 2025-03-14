fe02a2499a6b8695e9aa07ce9a70113fd777c302
udodirim <udodirimwisdom@gmail.com>
2025-03-14 11:37:44 +0100

    refactor CreateOrder and Order components; implement address fetching and loading state management

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -9,8 +9,8 @@ import { Form, redirect, useActionData, useNavigation } from 'react-router-dom';
 import store from '../../store';
 import { createOrder } from '../../services/apiRestaurant';
 import Button from '../../ui/Button';
-import { useSelector } from 'react-redux';
-import { getUser } from '../user/userSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchAddress, getUser } from '../user/userSlice';
 import { clearCart, getCartItems, getTotalCartPrice } from '../cart/cartSlice';
 import EmptyCart from '../cart/EmptyCart';
 import { formatCurrency } from '../../utils/helpers';
@@ -19,16 +19,24 @@ import { useState } from 'react';
 function CreateOrder() {
   const [withPriority, setWithPriority] = useState(false);
   const user = useSelector(getUser);
+  const {
+    status: addressStatus,
+    position,
+    address,
+    error: addressError,
+  } = useSelector((store) => store.user);
   const navigation = useNavigation();
   const isSubmitting = navigation.state === 'submitting';
 
   const name = user ? user : '';
   const formErrors = useActionData();
+  const dispatch = useDispatch();
 
   const cart = useSelector(getCartItems);
   const totalCartPrice = useSelector(getTotalCartPrice);
   const priorityPrice = withPriority ? totalCartPrice * 0.2 : 0;
   const totalPrice = totalCartPrice + priorityPrice;
+  const isFetching = addressStatus === 'loading';
 
   if (cart.length === 0) return <EmptyCart />;
 
@@ -63,16 +71,38 @@ function CreateOrder() {
           </div>
         </div>
 
-        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+        <div className="relative mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
           <label className="sm:basis-40">Address</label>
           <div className="grow">
             <input
               type="text"
               name="address"
+              defaultValue={address}
               required
               className="input w-full"
+              disabled={isFetching}
             />
+            {addressStatus === 'error' && (
+              <p className="mt-2 rounded bg-red-100 p-2 text-xs text-red-700">
+                {addressError}
+              </p>
+            )}
           </div>
+
+          {!position.latitude && !position.longitude && (
+            <span className="absolute right-[6px] top-0 z-30 rounded-lg shadow-lg shadow-stone-800 hover:shadow-stone-900 md:top-[6px]">
+              <Button
+                onClick={(e) => {
+                  e.preventDefault();
+                  dispatch(fetchAddress());
+                }}
+                btnStyle="small"
+                disabled={isFetching}
+              >
+                {isFetching ? 'getting position...' : 'Get position'}
+              </Button>
+            </span>
+          )}
         </div>
 
         <div className="mb-12 flex items-center gap-5">
@@ -91,7 +121,22 @@ function CreateOrder() {
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <Button type="submit" btnStyle="primary" disabled={isSubmitting}>
+          <input
+            type="hidden"
+            name="position"
+            value={
+              position.latitude && position.longitude
+                ? `${position.latitude},${position.longitude}`
+                : ''
+            }
+          />
+          <Button
+            type="submit"
+            btnStyle="primary"
+            disabled={
+              isSubmitting || (!position.latitude && !position.longitude)
+            }
+          >
             {isSubmitting
               ? 'Placing order...'
               : `Order now for ${formatCurrency(totalPrice)}`}
